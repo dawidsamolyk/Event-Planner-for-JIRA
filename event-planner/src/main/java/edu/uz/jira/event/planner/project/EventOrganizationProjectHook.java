94b6551cfe2a1d074c9764fa25e531e324aa9be2
Dawid Samołyk <dawid.samolyk@gmail.com>
2015-10-29 20:17:44 +0100

    LICENSE modification. Refactoring.

--- event-planner/src/main/java/edu/uz/jira/event/planner/project/EventOrganizationProjectHook.java
+++ event-planner/src/main/java/edu/uz/jira/event/planner/project/EventOrganizationProjectHook.java
@@ -1,10 +1,13 @@
-package edu.uz.jira.event.planner;
+package edu.uz.jira.event.planner.project;
 
 import com.atlassian.jira.blueprint.api.AddProjectHook;
 import com.atlassian.jira.blueprint.api.ConfigureData;
 import com.atlassian.jira.blueprint.api.ConfigureResponse;
 import com.atlassian.jira.blueprint.api.ValidateData;
 import com.atlassian.jira.blueprint.api.ValidateResponse;
+import com.atlassian.jira.issue.issuetype.IssueType;
+
+import java.util.Map;
 
 public class EventOrganizationProjectHook implements AddProjectHook {
     @Override
@@ -17,6 +20,9 @@ public class EventOrganizationProjectHook implements AddProjectHook {
     @Override
     public ConfigureResponse configure(final ConfigureData configureData) {
         String redirect = "/browse/" + configureData.project().getKey() + "#selectedTab=com.atlassian.jira.plugin.system.project%3Asummary-panel";
+
+        Map<String, IssueType> createdIssueTypes = configureData.createdIssueTypes();
+        
 
         ConfigureResponse configureResponse = ConfigureResponse.create().setRedirect(redirect);
 
