ff2235539d6a945a6ad36a9618ad809df9b48209
Dawid Samołyk <dawid.samolyk@gmail.com>
2015-12-10 15:39:05 +0100

    First steps in view (GUI) for event organization plans configuration.

--- event-planner/src/main/java/edu/uz/jira/event/planner/project/plan/EventPlanServlet.java
+++ event-planner/src/main/java/edu/uz/jira/event/planner/project/plan/EventPlanServlet.java
@@ -1,6 +1,6 @@
 package edu.uz.jira.event.planner.project.plan;
 
-import edu.uz.jira.event.planner.project.plan.model.Plan;
+import com.atlassian.templaterenderer.TemplateRenderer;
 
 import javax.annotation.Nonnull;
 import javax.servlet.ServletException;
@@ -8,43 +8,28 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public final class EventPlanServlet extends HttpServlet {
     private final EventOrganizationPlanService eventPlanService;
+    private final TemplateRenderer templateRenderer;
 
-    public EventPlanServlet(@Nonnull final EventOrganizationPlanService eventPlanService) {
+    public EventPlanServlet(@Nonnull final EventOrganizationPlanService eventPlanService, @Nonnull final TemplateRenderer templateRenderer) {
         this.eventPlanService = eventPlanService;
+        this.templateRenderer = templateRenderer;
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
-        final PrintWriter w = res.getWriter();
-        w.write("<h1>Event organization types</h1>");
-
-        w.write("<form method=\"post\">");
-        w.write("<input type=\"text\" name=\"name\" size=\"25\"/>");
-        w.write("&nbsp;&nbsp;");
-        w.write("<input type=\"submit\" name=\"submit\" value=\"Add\"/>");
-        w.write("</form>");
-
-        w.write("<ol>");
-
-        for (Plan eachEventPlan : eventPlanService.getAllPlans()) {
-            w.printf("<li>%s</li>", eachEventPlan.getName());
-        }
-
-        w.write("</ol>");
-        w.write("<script language='javascript'>document.forms[0].elements[0].focus();</script>");
-
-        w.close();
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        templateRenderer.render("/templates/admin/event-types.vm", response.getWriter());
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        final String name = req.getParameter("name");
-        eventPlanService.addPlanNamed(name);
+        //final String name = req.getParameter("name");
+        //eventPlanService.addPlanNamed(name);
 
         res.sendRedirect(req.getContextPath() + "/plugins/servlet/eventPlans/configuration");
     }
+
+
 }
