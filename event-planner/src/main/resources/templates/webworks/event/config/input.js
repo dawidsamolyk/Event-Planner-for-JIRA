f0ddb6636c57d707250ac7c21ab92bae7515d6b6
Dawid Samołyk <dawid.samolyk@gmail.com>
2016-01-13 13:03:29 +0100

    Little corrections.

--- event-planner/src/main/resources/templates/webworks/event/config/input.js
+++ event-planner/src/main/resources/templates/webworks/event/config/input.js
@@ -6,12 +6,12 @@ function getParameterByName(name) {
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
     results = regex.exec(location.search);
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
+};
 
 function isDueDateEmpty() {
     var dueDate = document.getElementById(DUE_DATE_ID).value;
     return dueDate.valueOf() == "".valueOf();
-}
+};
 
 function sendPostRequest() {
     var dueDateValue = document.getElementById(DUE_DATE_ID).value;
@@ -22,14 +22,14 @@ function sendPostRequest() {
                                         DUE_DATE_ID + "=" + dueDateValue +
                                         "&" +
                                         EVENT_TYPE_ID + "=" + eventTypeValue);
-}
+};
 
 function configureDatePicker() {
    jQuery('#event-duedate').datetimepicker({
        timepicker : true,
        format : 'd-m-Y H:i'
    });
-}
+};
 
 function configureDialog() {
     configureDatePicker();
@@ -44,4 +44,4 @@ function configureDialog() {
             $('#event-duedate-description').html('<div class="error" data-field="event-duedate">You must specify a date of the event.</div>');
         }
     });
-}
+};
